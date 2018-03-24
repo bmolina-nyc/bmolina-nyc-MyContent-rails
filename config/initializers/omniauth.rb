96163df31534487a7b2a34236a5d9ad7296b28f5
bmolina-nyc <bruce.molina.81@gmail.com>
2018-03-24 16:55:54 -0400

    adding oauth to new app

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :google_oauth2, "207489386440-dt656st17i8n6on5l8ansqqhl89eu83k.apps.googleusercontent.com", "0EmC5cW38v1CsUKchbFE3Fw6"
+  end
